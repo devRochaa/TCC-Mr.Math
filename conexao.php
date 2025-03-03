f8ab29b1ca04b440a5fe009a9875ed18676fdc6c
Daniel Rocha <124582086+devRochaa@users.noreply.github.com>
2025-03-02 21:26:56 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@ $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 $dbname = "bd_mrmath";
-$port = 3307;
+$port = 3306;
 $conexao = mysqli_connect($servidor, $usuario, $senha, $dbname, $port); // estabelece a conexão com o banco
 if (!$conexao) {
   die("Houve um erro: " . mysqli_connect_error());
